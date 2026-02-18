644560df1bfa4fd185db197e00de9c082179ac2d
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-18 17:11:30 +0100

    created basic pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutUsPage from "./pages/AboutUsPage";
+import ProductsPage from "./pages/ProductsPage";
+
 export default function App() {
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" Component={HomePage}></Route>
+        <Route path="/about-us" Component={AboutUsPage}></Route>
+        <Route path="/products" Component={ProductsPage}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
