b09b42e92e946e21c934ec54ef63f569bfe39d69
ManuelAliquo <manuelaliquo2103@gmail.com>
2026-02-19 20:55:19 +0100

    added products page redirect in case of non-existent product search

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
 export default function ProductDetailPage() {
   const { id } = useParams();
+  const navigate = useNavigate();
+
   const [product, setProduct] = useState({});
   const [showLoading, setShowLoading] = useState(true);
 
@@ -14,29 +16,33 @@ export default function ProductDetailPage() {
     });
   }, []);
 
-  const { title, price, category, image, rating, description } = product;
-
-  return showLoading ? (
-    <div className="d-flex align-items-center">
-      <h1>Loading</h1>
-      <div className="ms-3 spinner-border text-primary" role="status">
-        <span className="visually-hidden">Loading...</span>
+  if (showLoading)
+    return (
+      <div className="d-flex align-items-center">
+        <h1>Loading</h1>
+        <div className="ms-3 spinner-border text-primary" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
       </div>
-    </div>
-  ) : (
+    );
+
+  return product ? (
     <div className="row d-flex align-items-center gy-3 gx-5 my-4">
       <div className="col-12 col-sm-12 col-md-4 d-flex justify-content-center border rounded-4 p-3">
-        <img className="img-fluid" src={image} alt="Product" />
+        <img className="img-fluid" src={product.image} alt="Product" />
       </div>
       <div className="col-12 col-sm-12 col-md-8 d-flex flex-column gap-1 mb-3">
-        <h2>{title}</h2>
-        <div className="fs-5">{`${rating.rate}/5⭐ - (${rating.count})`}</div>
-        <div className="fs-4">{price}$</div>
-        <p>{description}</p>
+        <h2>{product.title}</h2>
+        <div>{product.category}</div>
+        <div className="fs-5">{`${product.rating.rate}/5⭐ - (${product.rating.count})`}</div>
+        <div className="fs-4">{product.price}$</div>
+        <p>{product.description}</p>
         <button className="btn btn-danger">
           ⚠️ Buy in a totally safe and not dangerous way!!! ⚠️
         </button>
       </div>
     </div>
+  ) : (
+    navigate("/products")
   );
 }
